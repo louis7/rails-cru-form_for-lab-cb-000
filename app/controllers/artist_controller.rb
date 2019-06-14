818c7e9f3c7bbb79b2ac48b1e0b1e2ac66ec3c80
louis baptiste <louisvis@ymail.com>
2019-06-14 05:31:36 +0000

    Automatically backed up by Learn

--- app/controllers/artist_controller.rb
+++ app/controllers/artist_controller.rb
@@ -1,5 +1,27 @@
-class ArtistController < ApplicationController
+class ArtistsController < ApplicationController
+
   def show
-    @artist =Artist.find(params[:id])
+    @artists = Artist.all
   end
+
+  def new
+    @artist = Artist.new
+  end
+
+  def create
+        @artist = Artist.create(name: params[:artist][:name], bio: params[:artist][:bio])
+        redirect_to artist_path(@artist)
+  end
+
+  def edit
+    @artist = Artist.find_by(params[:id])
+  end
+
+  def update
+    @artist = Artist.find_by(params[:id])
+    @artist.update(name: params[:artist][:name], bio: params[:artist][:bio])
+    redirect_to artist_path(@artist)
+
+  end
+
 end
